8485fc240be101b09605f5ae84e2bb689ca7ffe8
isuruij <2020uelog@gmail.com>
2023-08-07 14:04:18 +0530

    second commit

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 // creating a schema to create a model
 const Schema = mongoose.Schema;
@@ -15,10 +16,40 @@ const userSchema = new Schema({
     trim: true,
   },
   age: {
-    type: Number,
+    type: Number, 
     required: true,
   },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 });
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    throw new Error();
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error();
+  }
+
+  return user;
+};
 
 // creating a model using the schema
 const User = mongoose.model("Usernew", userSchema);
